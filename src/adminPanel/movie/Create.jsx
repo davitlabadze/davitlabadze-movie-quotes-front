3bb617cf827423ec11c0812b092a8f5b5679e230
davitlabadze <labadzedato18@gmail.com>
2022-03-24 20:41:27 +0400

    refactor(movie create): use useform

--- src/adminPanel/movie/Create.jsx
+++ src/adminPanel/movie/Create.jsx
@@ -4,35 +4,19 @@ import Table from '../../img/table.svg';
 import Eye from '../../img/eye.svg';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 function Create() {
-  const [movieEn, setMovieEn] = useState('');
-  const [movieKa, setMovieKa] = useState('');
-  const [formInputsValidaty, setFormInputsValidaty] = useState({
-    movieEN: true,
-    movieKA: true,
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const isEmpty = (value) => value.trim() === '';
-  const postData = (e) => {
-    e.preventDefault();
-    setMovieEn('');
-    setMovieKa('');
+  const onSubmit = (data) => {
     const formData = new FormData();
-    formData.append('movie-en', movieEn);
-    formData.append('movie-ka', movieKa);
-
-    const enterMovieEnIsValid = !isEmpty(movieEn);
-    const enterMovieKaIsValid = !isEmpty(movieKa);
-    setFormInputsValidaty({
-      movieEN: enterMovieEnIsValid,
-      movieKA: enterMovieKaIsValid,
-    });
-    const formIsValid = enterMovieEnIsValid && enterMovieKaIsValid;
-
-    if (!formIsValid) {
-      return;
-    }
+    formData.append('movie-en', data.movieEn);
+    formData.append('movie-ka', data.movieKa);
 
     axios
       .post('movies/create', formData, {
@@ -58,7 +42,7 @@ function Create() {
           <Link to={'/adminpanel/movies'}>All Data</Link>
         </button>
       </div>
-      <form className='mt-10'>
+      <form onSubmit={handleSubmit(onSubmit)} className='mt-10'>
         <div className='mb-6'>
           <label
             className='block mb-2 text-xs font-bold text-gray-700 uppercase'
@@ -67,19 +51,18 @@ function Create() {
             Movie_EN
           </label>
           <input
-            className={
-              !formInputsValidaty.movieEN
-                ? 'w-full p-2 border-2 border-red-700 rounded'
-                : 'w-full p-2 border border-gray-400 rounded outline-blue-700'
-            }
+            className={`w-full p-2 border border-gray-400 rounded outline-none ${
+              errors.movieEn && 'w-full p-2 border-2 border-red-700 rounded'
+            }`}
             type='text'
             name='movie-en'
             id='movie-en'
-            value={movieEn}
-            onChange={(e) => setMovieEn(e.target.value)}
+            {...register('movieEn', { required: 'Value is required' })}
           />
-          {!formInputsValidaty.movieEN && (
-            <p className='mt-2 text-xs text-red-500'>Value is required</p>
+          {errors.movieEn && (
+            <p className='mt-2 text-xs text-red-500'>
+              {errors.quoteEn.message}
+            </p>
           )}
         </div>
 
@@ -91,25 +74,23 @@ function Create() {
             Movie_KA
           </label>
           <input
-            className={
-              !formInputsValidaty.movieKA
-                ? 'w-full p-2 border-2 border-red-700 rounded'
-                : 'w-full p-2 border border-gray-400 rounded outline-blue-700'
-            }
+            className={`w-full p-2 border border-gray-400 rounded outline-none ${
+              errors.movieKa && 'w-full p-2 border-2 border-red-700 rounded'
+            }`}
             type='text'
             name='movie-ka'
             id='movie-ka'
-            value={movieKa}
-            onChange={(e) => setMovieKa(e.target.value)}
+            {...register('movieKa', { required: 'Value is required' })}
           />
-          {!formInputsValidaty.movieKA && (
-            <p className='mt-2 text-xs text-red-500'>Value is required</p>
+          {errors.movieKa && (
+            <p className='mt-2 text-xs text-red-500'>
+              {errors.quoteKa.message}
+            </p>
           )}
         </div>
 
         <div className='flex mb-6 w-min'>
           <button
-            onClick={postData}
             type='submit'
             className='w-full px-4 py-2 text-white bg-green-600 rounded-lg rounderd hover:bg-green-700'
           >
