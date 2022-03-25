089fb4314391a482e660906a5072d803ccefbe47
davitlabadze <labadzedato18@gmail.com>
2022-03-25 18:32:05 +0400

    rfactor(quote update): use useForm

--- src/adminPanel/movie/Update.jsx
+++ src/adminPanel/movie/Update.jsx
@@ -1,22 +1,23 @@
 import React, { useEffect, useState, Fragment } from 'react';
 import axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 import Table from '../../img/table.svg';
 import Eye from '../../img/eye.svg';
 
 function Update() {
   const [id, setID] = useState(null);
-  const [movieEn, setMovieEn] = useState('');
-  const [movieKa, setMovieKa] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [editMovie, setEditMovie] = useState();
-  const [formInputsValidaty, setFormInputsValidaty] = useState({
-    movieEN: true,
-    movieKA: true,
-  });
 
-  const isEmpty = (value) => value.trim() === '';
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
+
   const params = useParams();
 
   useEffect(() => {
@@ -35,33 +36,21 @@ function Update() {
       .then((res) => {
         setEditMovie(res.data);
         setID(res.data.id);
-        setMovieEn(res.data.movie.en);
-        setMovieKa(res.data.movie.ka);
+        setValue('movieEn', res.data.movie.en);
+        setValue('movieKa', res.data.movie.ka);
         console.log(res.data);
       })
       .catch((err) => console.log(err));
     setIsLoading(false);
   };
 
-  const updateMovie = (e) => {
-    e.preventDefault();
-    const enterMovieEnIsValid = !isEmpty(movieEn);
-    const enterMovieKaIsValid = !isEmpty(movieKa);
-    setFormInputsValidaty({
-      movieEN: enterMovieEnIsValid,
-      movieKA: enterMovieKaIsValid,
-    });
-    const formIsValid = enterMovieEnIsValid && enterMovieKaIsValid;
-
-    if (!formIsValid) {
-      return;
-    }
+  const updateMovie = (data) => {
     axios
       .put(
         `movies/${id}/edit`,
         {
-          'movie-en': movieEn,
-          'movie-ka': movieKa,
+          'movie-en': data.movieEn,
+          'movie-ka': data.movieKa,
         },
         {
           headers: {
@@ -93,10 +82,10 @@ function Update() {
             </p>
             <button className='flex p-2 text-white bg-gray-500 hover:bg-gray-600 rounded-xl'>
               <img className='w-6 h-6' src={Eye} alt='eye' />
-              <Link to={'/adminpanel/movies'}>All Data</Link>
+              <Link to={'/adminpanel/movies'}>All Movies</Link>
             </button>
           </div>
-          <form className='mt-10'>
+          <form onSubmit={handleSubmit(updateMovie)} className='mt-10'>
             <div className='mb-6'>
               <label
                 className='block mb-2 text-xs font-bold text-gray-700 uppercase'
@@ -105,19 +94,20 @@ function Update() {
                 Movie (EN)
               </label>
               <input
-                className={
-                  !formInputsValidaty.movieEN
-                    ? 'w-full p-2 border-2 border-red-700 rounded'
-                    : 'w-full p-2 border border-gray-400 rounded outline-blue-700'
-                }
+                className={`w-full p-2 border border-gray-400 rounded outline-none ${
+                  errors.movieEn && 'w-full p-2 border-2 border-red-700 rounded'
+                }`}
                 type='text'
                 name='movie-en'
                 id='movie-en'
-                onChange={(e) => setMovieEn(e.target.value)}
-                value={movieEn}
+                {...register('movieEn', {
+                  required: 'Value is required',
+                })}
               />
-              {!formInputsValidaty.movieEN && (
-                <p className='mt-2 text-xs text-red-500'>Value is required</p>
+              {errors.movieEn && (
+                <p className='mt-2 text-xs text-red-500'>
+                  {errors.movieEn.message}
+                </p>
               )}
             </div>
 
@@ -129,19 +119,18 @@ function Update() {
                 Movie (KA)
               </label>
               <input
-                className={
-                  !formInputsValidaty.movieKA
-                    ? 'w-full p-2 border-2 border-red-700 rounded'
-                    : 'w-full p-2 border border-gray-400 rounded outline-blue-700'
-                }
+                className={`w-full p-2 border border-gray-400 rounded outline-none ${
+                  errors.movieKa && 'w-full p-2 border-2 border-red-700 rounded'
+                }`}
                 type='text'
                 name='movie-ka'
                 id='movie-ka'
-                value={movieKa}
-                onChange={(e) => setMovieKa(e.target.value)}
+                {...register('movieKa', { required: 'Value is required' })}
               />
-              {!formInputsValidaty.movieKA && (
-                <p className='mt-2 text-xs text-red-500'>Value is required</p>
+              {errors.movieKa && (
+                <p className='mt-2 text-xs text-red-500'>
+                  {errors.movieKa.message}
+                </p>
               )}
             </div>
             <div className='mb-6 w-min'>
