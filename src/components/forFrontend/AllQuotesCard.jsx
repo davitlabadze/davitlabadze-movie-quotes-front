c2443a55a4d17a12ccdf1d2cf696dae6189c68de
davitlabadze <labadzedato18@gmail.com>
2022-03-28 22:59:57 +0400

    refactor(movie quotes): show movie quotes

--- src/components/forFrontend/AllQuotesCard.jsx
+++ src/components/forFrontend/AllQuotesCard.jsx
@@ -16,17 +16,20 @@ function Card() {
     getMovieQuotes();
   }, []);
 
-  const getMovieQuotes = () => {
+  const getMovieQuotes = async () => {
     setIsLoading(true);
-    axios
-      .get(`get-quotes/${params.movieId}`)
-      .then((res) => {
-        setQuotes(res.data);
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    try {
+      await axios
+        .get(`get-quotes/${params.movieId}`)
+        .then((res) => {
+          setQuotes(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (err) {
+      console.error(err);
+    }
     setIsLoading(false);
   };
 
@@ -35,20 +38,22 @@ function Card() {
       {!isLoading && quotes && (
         <div>
           <Header title={quotes.movie[i18n.language]} />
-          <div className='flex justify-center mt-14 '>
-            <div className='bg-white rounded-lg '>
-              <div className=' h-96'>
-                <img
-                  className='object-cover w-full h-full rounded'
-                  src={`http://localhost:8000/storage/${quotes.quotes[0].thumbnail}`}
-                  alt='img'
-                />
+          {quotes.quotes.map((quote) => (
+            <div className='flex justify-center mt-14 '>
+              <div className='bg-white rounded-lg '>
+                <div className=' h-96'>
+                  <img
+                    className='object-cover w-full h-full rounded'
+                    src={`http://localhost:8000/storage/${quote.thumbnail}`}
+                    alt='img'
+                  />
+                </div>
+                <h1 className='py-16 text-5xl text-center text-black'>
+                  {quote.quote[i18n.language]}
+                </h1>
               </div>
-              <h1 className='py-16 text-5xl text-center text-black'>
-                {quotes.quotes[0].quote[i18n.language]}
-              </h1>
             </div>
-          </div>
+          ))}
         </div>
       )}
       {isLoading && (
