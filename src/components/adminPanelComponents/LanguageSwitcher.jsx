b0d484e67b8837351b7ee27f0f3a898ce78af3ac
davitlabadze <labadzedato18@gmail.com>
2022-04-27 11:59:08 +0400

    refactor(language switcher): add style

--- src/components/adminPanelComponents/LanguageSwitcher.jsx
+++ src/components/adminPanelComponents/LanguageSwitcher.jsx
@@ -6,13 +6,17 @@ function LanguageSwitcher() {
     <div>
       <div>
         <button
-          className='w-10 h-10 text-white bg-gray-800 rounded-md hover:bg-gray-900'
+          className={`w-10 h-10 text-white bg-gray-800 rounded-md hover:bg-gray-900 hover:text-green-600 +
+            ${i18next.language === 'en' ? 'text-gray-800 bg-white' : ''}
+          `}
           onClick={() => i18next.changeLanguage('en')}
         >
           en
         </button>
         <button
-          className='w-10 h-10 ml-2 text-white bg-gray-800 rounded-md hover:bg-gray-900'
+          className={`w-10 h-10 ml-2 text-white bg-gray-800 rounded-md hover:bg-gray-900 hover:text-green-600 +
+          ${i18next.language === 'ka' ? 'text-gray-800 bg-white' : ''}
+        `}
           onClick={() => i18next.changeLanguage('ka')}
         >
           ka
--- src/components/frontendComponents/LanguageSwitcher.jsx
+++ src/components/frontendComponents/LanguageSwitcher.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import i18next from 'i18next';
-import i18n from 'i18next';
 import { useTranslation } from 'react-i18next';
 
 function LanguageSwitcher() {
@@ -9,7 +8,7 @@ function LanguageSwitcher() {
     <div className='fixed px-12 space-y-3 py-80'>
       <div
         className={`w-12 h-12 px-3 py-3 mb-2 text-sm text-center  border-2 rounded-full cursor-pointer +
-          ${i18n.language === 'en' ? 'text-white' : 'bg-gray-50'}
+          ${i18next.language === 'en' ? 'text-white' : 'bg-gray-50'}
         `}
       >
         <button onClick={() => i18next.changeLanguage('en')}>en</button>
@@ -17,7 +16,7 @@ function LanguageSwitcher() {
 
       <div
         className={`w-12 h-12 px-3 py-3 text-sm text-center  border-2 rounded-full cursor-pointer +
-          ${i18n.language === 'ka' ? 'text-white' : 'bg-gray-50'}
+          ${i18next.language === 'ka' ? 'text-white' : 'bg-gray-50'}
         `}
       >
         <button onClick={() => i18next.changeLanguage('ka')}>ka</button>
